053c91b20332124864345678a296868834f8fd39
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-11-12 12:20:53 -0500

    yellow line tip now working

--- javascripts/mr-line.js
+++ javascripts/mr-line.js
@@ -76,12 +76,12 @@ data = d3.dsv(",", "output/logisticreg-mr.csv", function(d) {
   svg1.append("rect").attr("x", width + 20).attr("y", 0).attr("height", 12).attr("width", 24).style("fill", "#FFC300")
   svg1.append("text").attr("x", width + 50).attr("y", 10).text("mr").style("font-size", 12).style("font-weight", "bold")
 
-  data = d3.dsv(",", "output/logisticreg-dblp.csv", function(d) {
+  data_new = d3.dsv(",", "output/logisticreg-dblp.csv", function(d) {
             return {
                 x: +d["x"],
                 y: +d["y"]
             };
-        }).then(function(data) {
+        }).then(function(data_new) {
 
   var svg2 = d3.select("#mr-line-dblp")
 
@@ -90,17 +90,17 @@ data = d3.dsv(",", "output/logisticreg-mr.csv", function(d) {
       .y(function(d) {return yScale(d.y-0.0255); }); // set the y values for the line generator
 
   svg2.append("path")
-      .datum(data) // 10. Binds data to the line
+      .datum(data_new) // 10. Binds data_new to the line
       .attr("class", "line") // Assign a class for styling
       .attr("fill", "none")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 2)
       .attr("d", line_new) // 11. Calls the line generator
 
-  tip = d3.tip().attr('class', 'd3-tip').html(function(d) {temp = 0; temp2 = 0; data.forEach(function(da) {if (da.x == d.x && da.y == d.y) {temp = da.x; temp2 = da.y;};}); return "<div>x: " + temp + "</div><div>y: " + temp2 + "</div>"; });
-    svg2.call(tip);
+  tip2 = d3.tip().attr('class', 'd3-tip').html(function(d) {temp = 0; temp2 = 0; data_new.forEach(function(da) {if (da.x == d.x && da.y == d.y) {temp = da.x; temp2 = da.y;};}); return "<div>x: " + temp + "</div><div>y: " + temp2 + "</div>"; });
+    svg2.call(tip2);
 
-  svg2.selectAll().data(data).enter().append("circle").attr("id", function(d, i) { return d.x + '-' + d.y}).attr("class", "dot").attr("cx", function(d, i) { return xScale(d.x+0.193); }).attr("cy", function(d, i) { return yScale(d.y-0.0255) }).style("fill", "steelblue").attr("r", 5).on("mouseover", tip.show).on("mouseout", tip.hide);
+  svg2.selectAll().data(data_new).enter().append("circle").attr("id", function(d, i) { return d.x + '-' + d.y}).attr("class", "dot").attr("cx", function(d, i) { return xScale(d.x+0.193); }).attr("cy", function(d, i) { return yScale(d.y-0.0255) }).style("fill", "steelblue").attr("r", 5).on("mouseover", tip2.show).on("mouseout", tip2.hide);
 
   svg2.append("rect").attr("x", width + 120).attr("y", 120).attr("height", 12).attr("width", 24).style("fill", "steelblue")
   svg2.append("text").attr("x", width + 150).attr("y", 130).text("dblp").style("font-size", 12).style("font-weight", "bold")
