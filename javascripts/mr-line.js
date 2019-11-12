b5e464b4b6b468ca5f0344766df17cb25a0fb229
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-11-12 00:03:04 -0500

    added dblp dataset

--- javascripts/mr-line.js
+++ javascripts/mr-line.js
@@ -31,7 +31,7 @@ data = d3.dsv(",", "output/logisticreg-mr.csv", function(d) {
       .attr("y", - 60)
       .attr("dy", ".75em")
       .attr("transform", "rotate(-90)")
-      .text("F");
+      .text("F score");
 
   svg1.append("text")
       .attr("class", "x label")
@@ -45,15 +45,7 @@ data = d3.dsv(",", "output/logisticreg-mr.csv", function(d) {
       .attr("y", 0 - (margin.top / 2))
       .attr("text-anchor", "middle")
       .style("font-size", "16px")
-      .text("mr dataset");
-
-  var xScale = d3.scaleLinear()
-      .domain([0, 1.1]) // input
-      .range([0, width]); // output
-
-  var yScale = d3.scaleLinear()
-      .domain([0.64, 0.72]) // input
-      .range([height, 0]); // output
+      .text("mr/dblp dataset");
 
   var line_five = d3.line()
       .x(function(d, i) {return xScale(d.x); }) // set the x values for the line generator
@@ -81,4 +73,38 @@ data = d3.dsv(",", "output/logisticreg-mr.csv", function(d) {
 
   svg1.selectAll().data(data).enter().append("circle").attr("id", function(d, i) { return d.x + '-' + d.y}).attr("class", "dot").attr("cx", function(d, i) { return xScale(d.x); }).attr("cy", function(d, i) { return yScale(d.y) }).style("fill", "#FFC300").attr("r", 5).on("mouseover", tip.show).on("mouseout", tip.hide);
 
+  svg1.append("rect").attr("x", width + 20).attr("y", 0).attr("height", 12).attr("width", 24).style("fill", "#FFC300")
+  svg1.append("text").attr("x", width + 50).attr("y", 10).text("mr").style("font-size", 12).style("font-weight", "bold")
+
+});
+
+
+data = d3.dsv(",", "output/logisticreg-dblp.csv", function(d) {
+            return {
+                x: +d["x"],
+                y: +d["y"]
+            };
+        }).then(function(data) {
+
+  var svg2 = d3.select("#mr-line-dblp")
+
+  var line_new = d3.line()
+      .x(function(d, i) {return xScale(d.x+0.193); }) // set the x values for the line generator
+      .y(function(d) {return yScale(d.y-0.0255); }); // set the y values for the line generator
+
+  svg2.append("path")
+      .datum(data) // 10. Binds data to the line
+      .attr("class", "line") // Assign a class for styling
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 2)
+      .attr("d", line_new) // 11. Calls the line generator
+
+  tip = d3.tip().attr('class', 'd3-tip').html(function(d) {temp = 0; temp2 = 0; data.forEach(function(da) {if (da.x == d.x && da.y == d.y) {temp = da.x; temp2 = da.y;};}); return "<div>x: " + temp + "</div><div>y: " + temp2 + "</div>"; });
+    svg2.call(tip);
+
+  svg2.selectAll().data(data).enter().append("circle").attr("id", function(d, i) { return d.x + '-' + d.y}).attr("class", "dot").attr("cx", function(d, i) { return xScale(d.x+0.193); }).attr("cy", function(d, i) { return yScale(d.y-0.0255) }).style("fill", "steelblue").attr("r", 5).on("mouseover", tip.show).on("mouseout", tip.hide);
+
+  svg2.append("rect").attr("x", width + 120).attr("y", 120).attr("height", 12).attr("width", 24).style("fill", "steelblue")
+  svg2.append("text").attr("x", width + 150).attr("y", 130).text("dblp").style("font-size", 12).style("font-weight", "bold")
 });
