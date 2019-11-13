5219b10d66f107e019d73009915d3b1c2b5292b0
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-11-12 22:52:13 -0500

    determine the axis range in scatter plot

--- javascripts/scatter-mr.js
+++ javascripts/scatter-mr.js
@@ -10,4 +10,28 @@ data = d3.dsv(",", "output/embed-vis-mr-train.csv", function(d) {
             };
         }).then(function(data) {
 
+  var min_x = 0, max_x = 0, min_y = 0, max_y = 0;
+
+  data.forEach(function(point) {
+    if (min_x === 0) {
+      min_x = point.x;
+      min_y = point.y;
+      max_x = point.x;
+      max_y = point.y;
+    } else {
+      if (point.x > max_x) {
+        max_x = point.x;
+      }
+      if (point.x < min_x) {
+        min_x = point.x;
+      }
+      if (point.y > max_y) {
+        max_y = point.y;
+      }
+      if (point.y < min_y) {
+        min_y = point.y;
+      }
+    }
+  });
+
 });
