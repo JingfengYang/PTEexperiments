cd445a57d489d83a6386c2d06a0edce5dbbe1e7f
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-11-12 22:51:28 -0500

    dblp scatter plot js

---
+++ javascripts/scatter-dblp.js
@@ -0,0 +1,13 @@
+var margin = {top: 100, right: 100, bottom: 100, left: 100}
+  , width = window.innerWidth * 7 / 10 - margin.left - margin.right
+  , height = window.innerHeight * 3 / 4 - margin.top - margin.bottom;
+
+data = d3.dsv(",", "output/embed-vis-dblp-train.csv", function(d) {
+            return {
+                x: +d["x"],
+                y: +d["y"],
+                label: +d["label"]
+            };
+        }).then(function(data) {
+
+});
