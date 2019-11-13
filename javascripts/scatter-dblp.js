8f98b456c60b7d20aa5b783c4fc9da3c1fbb0ed0
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-11-12 22:52:53 -0500

    scatter plot js

--- javascripts/scatter-dblp.js
+++ javascripts/scatter-dblp.js
@@ -33,4 +33,60 @@ data = d3.dsv(",", "output/embed-vis-dblp-train.csv", function(d) {
       }
     }
   });
+
+
+  var xScale = d3.scaleLinear()
+      .domain([min_x, max_x])
+      .range([0, width]);
+
+  var yScale = d3.scaleLinear()
+      .domain([min_y, max_y])
+      .range([height, 0]);
+
+var color = ["#a8ddb5","#7bccc4","#4eb3d3","#2b8cbe","#0868ac","#084081"];
+
+  var cScale = d3.scaleQuantize().domain([1, 6]).range(color);
+
+  var svg1 = d3.select("#scatter-dblp").append("svg")
+      .attr("id", "dblp-line-dblp")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg1.append("text")
+      .attr("class", "y label")
+      .attr("text-anchor", "end")
+      .attr("x", - height / 2)
+      .attr("y", - 60)
+      .attr("dy", ".75em")
+      .attr("transform", "rotate(-90)")
+      .text("Component 2");
+
+  svg1.append("text")
+      .attr("class", "x label")
+      .attr("text-anchor", "middle")
+      .attr("x", width / 2)
+      .attr("y", height + 40)
+      .text("Component 1");
+
+  svg1.append("text")
+      .attr("x", (width / 2))
+      .attr("y", 0 - (margin.top / 2))
+      .attr("text-anchor", "middle")
+      .style("font-size", "16px")
+      .text("dblp dataset");
+
+  var line_five = d3.line()
+      .x(function(d, i) {return xScale(d.x); }) // set the x values for the line generator
+      .y(function(d) {return yScale(d.y); }); // set the y values for the line generator
+
+  svg1.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+
+  svg1.append("g")
+      .attr("class", "y axis")
+      .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
 });
