b1cb5e5d6bcce49e078abc70ffaa3011afe2c672
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-11-15 21:42:48 -0500

    added mr word-label and word-word

---
+++ javascripts/word-label-mr.js
@@ -0,0 +1,414 @@
+data_sidf = [
+  { "source": "ludicrous", "target": "terms", "weight": 1 },
+  { "source": "ludicrous", "target": "college", "weight": 1 },
+  { "source": "ludicrous", "target": "borderline", "weight": 1 },
+  { "source": "ludicrous", "target": "harvard", "weight": 1 },
+  { "source": "ludicrous", "target": "laugh-out-loud", "weight": 1 },
+  { "source": "ludicrous", "target": "insulting", "weight": 1 },
+  { "source": "ludicrous", "target": "loopy", "weight": 1 },
+  { "source": "ludicrous", "target": "carl", "weight": 1 },
+  { "source": "ludicrous", "target": "moat", "weight": 1 },
+  { "source": "sequence", "target": "issue", "weight": 1 },
+  { "source": "sequence", "target": "protracted", "weight": 1 },
+  { "source": "sequence", "target": "borderline", "weight": 1 },
+  { "source": "sequence", "target": "brutal", "weight": 1 },
+  { "source": "sequence", "target": "stirring", "weight": 1 },
+  { "source": "sequence", "target": "extended", "weight": 1 },
+  { "source": "sequence", "target": "bedroom", "weight": 1 },
+  { "source": "sequence", "target": "pre-credit", "weight": 1 },
+  { "source": "sequence", "target": "judith", "weight": 1 },
+  { "source": "sequence", "target": "zaza's", "weight": 1 },
+  { "source": "sequence", "target": "executed", "weight": 1 },
+  { "source": "sequence", "target": "fluid", "weight": 1 },
+  { "source": "sequence", "target": "dramaturgy", "weight": 1 },
+  { "source": "sequence", "target": "surge", "weight": 1 },
+  { "source": "sequence", "target": "swirling", "weight": 1 },
+  { "source": "soundtrack", "target": "rap", "weight": 1 },
+  { "source": "soundtrack", "target": "tended", "weight": 1 },
+  { "source": "soundtrack", "target": "cumbersome", "weight": 1 },
+  { "source": "soundtrack", "target": "simpering", "weight": 1 },
+  { "source": "soundtrack", "target": "stirring", "weight": 1 },
+  { "source": "soundtrack", "target": "songs", "weight": 1 },
+  { "source": "soundtrack", "target": "italian-language", "weight": 1 },
+  { "source": "soundtrack", "target": "throws", "weight": 1 },
+  { "source": "soundtrack", "target": "blues", "weight": 1 },
+  { "source": "soundtrack", "target": "piano", "weight": 1 },
+  { "source": "soundtrack", "target": "beach", "weight": 1 },
+  { "source": "soundtrack", "target": "outstanding", "weight": 1 },
+  { "source": "soundtrack", "target": "strings", "weight": 1 },
+  { "source": "soundtrack", "target": "drown", "weight": 1 },
+  { "source": "soundtrack", "target": "tinny", "weight": 1 },
+  { "source": "soundtrack", "target": "fish-out-of-water", "weight": 1 },
+  { "source": "soundtrack", "target": "arc", "weight": 1 },
+  { "source": "soundtrack", "target": "ratchets", "weight": 1 },
+  { "source": "soundtrack", "target": "ear-pleasing", "weight": 1 },
+  { "source": "soundtrack", "target": "unending", "weight": 1 },
+  { "source": "interpretation", "target": "correct", "weight": 1 },
+  { "source": "interpretation", "target": "also", "weight": 1 },
+  { "source": "interpretation", "target": "actually", "weight": 1 },
+  { "source": "interpretation", "target": "was", "weight": 1 },
+  { "source": "interpretation", "target": "rewarding", "weight": 1 },
+  { "source": "interpretation", "target": "shouldn't", "weight": 1 },
+  { "source": "interpretation", "target": "troubling", "weight": 1 },
+  { "source": "interpretation", "target": "ecclesiastes", "weight": 1 },
+  { "source": "ensnared", "target": "version", "weight": 1 },
+  { "source": "ensnared", "target": "without", "weight": 1 },
+  { "source": "ensnared", "target": "becoming", "weight": 1 },
+  { "source": "ensnared", "target": "marvel", "weight": 1 },
+  { "source": "plotting", "target": "insultingly", "weight": 1 },
+  { "source": "plotting", "target": "unbelievable", "weight": 1 },
+  { "source": "plotting", "target": "old-fashioned", "weight": 1 },
+  { "source": "plotting", "target": "crisis", "weight": 1 },
+  { "source": "plotting", "target": "stereotyped", "weight": 1 },
+  { "source": "plotting", "target": "ensures", "weight": 1 },
+  { "source": "plotting", "target": "hole-ridden", "weight": 1 },
+  { "source": "plotting", "target": "legitimate", "weight": 1 },
+  { "source": "provocative", "target": "nothing", "weight": 2 },
+  { "source": "nothing", "target": "leave", "weight": 1 },
+  { "source": "nothing", "target": "hollywood", "weight": 1 },
+  { "source": "nothing", "target": "obviously", "weight": 1 },
+  { "source": "nothing", "target": "match", "weight": 1 },
+  { "source": "nothing", "target": "commercial", "weight": 1 },
+  { "source": "nothing", "target": "enjoy", "weight": 1 },
+  { "source": "nothing", "target": "ride", "weight": 1 },
+  { "source": "nothing", "target": "five", "weight": 1 },
+  { "source": "nothing", "target": "pretty", "weight": 1 },
+  { "source": "nothing", "target": "summer", "weight": 2 },
+  { "source": "nothing", "target": "unfaithful", "weight": 1 },
+  { "source": "nothing", "target": "else", "weight": 8 },
+  { "source": "nothing", "target": "becomes", "weight": 1 },
+  { "source": "nothing", "target": "mediocre", "weight": 1 },
+  { "source": "nothing", "target": "away", "weight": 1 },
+  { "source": "nothing", "target": "happens", "weight": 3 },
+  { "source": "nothing", "target": "sincere", "weight": 1 },
+  { "source": "nothing", "target": "show", "weight": 4 },
+  { "source": "nothing", "target": "happening", "weight": 1 },
+  { "source": "nothing", "target": "something's", "weight": 1 },
+  { "source": "nothing", "target": "forgettable", "weight": 1 },
+  { "source": "nothing", "target": "sea", "weight": 1 },
+  { "source": "nothing", "target": "everything", "weight": 1 },
+  { "source": "nothing", "target": "drink", "weight": 1 },
+  { "source": "nothing", "target": "extravaganza", "weight": 1 },
+  { "source": "nothing", "target": "filmmaking", "weight": 1 },
+  { "source": "nothing", "target": "revealing", "weight": 2 },
+  { "source": "nothing", "target": "diane", "weight": 1 },
+  { "source": "nothing", "target": "lane", "weight": 1 },
+  { "source": "nothing", "target": "compared", "weight": 1 },
+  { "source": "nothing", "target": "fresh", "weight": 1 },
+  { "source": "nothing", "target": "whatsoever", "weight": 1 },
+  { "source": "nothing", "target": "wonderful", "weight": 1 },
+  { "source": "nothing", "target": "audience", "weight": 1 },
+  { "source": "nothing", "target": "belt", "weight": 1 },
+  { "source": "nothing", "target": "phony", "weight": 1 },
+  { "source": "nothing", "target": "represents", "weight": 1 },
+  { "source": "nothing", "target": "offering", "weight": 1 },
+  { "source": "nothing", "target": "savage", "weight": 1 },
+  { "source": "nothing", "target": "garden", "weight": 1 },
+  { "source": "nothing", "target": "children", "weight": 1 },
+  { "source": "nothing", "target": "happen", "weight": 1 },
+  { "source": "nothing", "target": "hadn't", "weight": 1 },
+  { "source": "nothing", "target": "popcorn", "weight": 2 },
+  { "source": "nothing", "target": "add", "weight": 1 },
+  { "source": "nothing", "target": "desperately", "weight": 1 },
+  { "source": "nothing", "target": "appeal", "weight": 1 },
+  { "source": "nothing", "target": "pleasant", "weight": 1 },
+  { "source": "nothing", "target": "relatively", "weight": 1 },
+  { "source": "nothing", "target": "contrived", "weight": 1 },
+  { "source": "nothing", "target": "tepid", "weight": 1 },
+  { "source": "nothing", "target": "convenient", "weight": 1 },
+  { "source": "nothing", "target": "ambiguous", "weight": 1 },
+  { "source": "nothing", "target": "masterpiece", "weight": 1 },
+  { "source": "nothing", "target": "raunchy", "weight": 1 },
+  { "source": "nothing", "target": "cipher", "weight": 1 },
+  { "source": "nothing", "target": "schwarzenegger", "weight": 1 },
+  { "source": "nothing", "target": "started", "weight": 1 },
+  { "source": "nothing", "target": "horror", "weight": 1 },
+  { "source": "nothing", "target": "sexy", "weight": 1 },
+  { "source": "nothing", "target": "reveal", "weight": 1 },
+  { "source": "nothing", "target": "graphic", "weight": 1 },
+  { "source": "nothing", "target": "dana", "weight": 1 },
+  { "source": "nothing", "target": "carvey", "weight": 1 },
+  { "source": "nothing", "target": "deep", "weight": 1 },
+  { "source": "nothing", "target": "formula", "weight": 1 },
+  { "source": "nothing", "target": "zone", "weight": 1 },
+  { "source": "nothing", "target": "suggests", "weight": 1 },
+  { "source": "nothing", "target": "interested", "weight": 1 }
+];
+
+labels_sidf = [
+  { "source": 1, "target": "nothing" },
+  { "source": 1, "target": "version" },
+  { "source": 0, "target": "plotting" },
+  { "source": 0, "target": "ludicrous" },
+  { "source": 0, "target": "provocative" },
+  { "source": 0, "target": "without" },
+  { "source": 0, "target": "correct" },
+  { "source": 1, "target": "also" },
+  { "source": 1, "target": "actually" },
+  { "source": 0, "target": "was" },
+  { "source": 1, "target": "sequence" },
+  { "source": 1, "target": "leave" },
+  { "source": 1, "target": "hollywood" },
+  { "source": 0, "target": "obviously" },
+  { "source": 1, "target": "match" },
+  { "source": 0, "target": "commercial" },
+  { "source": 0, "target": "enjoy" },
+  { "source": 0, "target": "ride" },
+  { "source": 1, "target": "five" },
+  { "source": 0, "target": "pretty" },
+  { "source": 0, "target": "summer" },
+  { "source": 0, "target": "unfaithful" },
+  { "source": 1, "target": "else" },
+  { "source": 0, "target": "becomes" },
+  { "source": 0, "target": "mediocre" },
+  { "source": 0, "target": "away" },
+  { "source": 0, "target": "happens" },
+  { "source": 0, "target": "becoming" },
+  { "source": 0, "target": "sincere" },
+  { "source": 1, "target": "show" },
+  { "source": 0, "target": "happening" },
+  { "source": 0, "target": "something's" },
+  { "source": 0, "target": "forgettable" },
+  { "source": 1, "target": "sea" },
+  { "source": 1, "target": "everything" },
+  { "source": 1, "target": "drink" },
+  { "source": 1, "target": "extravaganza" },
+  { "source": 1, "target": "filmmaking" },
+  { "source": 1, "target": "revealing" },
+  { "source": 0, "target": "diane" },
+  { "source": 0, "target": "lane" },
+  { "source": 1, "target": "compared" },
+  { "source": 1, "target": "fresh" },
+  { "source": 0, "target": "whatsoever" },
+  { "source": 1, "target": "wonderful" },
+  { "source": 1, "target": "audience" },
+  { "source": 0, "target": "belt" },
+  { "source": 0, "target": "phony" },
+  { "source": 1, "target": "represents" },
+  { "source": 0, "target": "offering" },
+  { "source": 0, "target": "savage" },
+  { "source": 0, "target": "garden" },
+  { "source": 0, "target": "children" },
+  { "source": 1, "target": "happen" },
+  { "source": 1, "target": "hadn't" },
+  { "source": 1, "target": "popcorn" },
+  { "source": 1, "target": "add" },
+  { "source": 0, "target": "desperately" },
+  { "source": 1, "target": "appeal" },
+  { "source": 1, "target": "rewarding" },
+  { "source": 0, "target": "pleasant" },
+  { "source": 0, "target": "relatively" },
+  { "source": 0, "target": "contrived" },
+  { "source": 0, "target": "tepid" },
+  { "source": 1, "target": "convenient" },
+  { "source": 1, "target": "ambiguous" },
+  { "source": 0, "target": "masterpiece" },
+  { "source": 0, "target": "raunchy" },
+  { "source": 0, "target": "cipher" },
+  { "source": 0, "target": "schwarzenegger" },
+  { "source": 0, "target": "started" },
+  { "source": 1, "target": "horror" },
+  { "source": 0, "target": "sexy" },
+  { "source": 0, "target": "insultingly" },
+  { "source": 0, "target": "unbelievable" },
+  { "source": 1, "target": "reveal" },
+  { "source": 0, "target": "graphic" },
+  { "source": 0, "target": "dana" },
+  { "source": 0, "target": "carvey" },
+  { "source": 1, "target": "deep" },
+  { "source": 0, "target": "formula" },
+  { "source": 0, "target": "zone" },
+  { "source": 1, "target": "suggests" },
+  { "source": 1, "target": "interested" },
+  { "source": 0, "target": "old-fashioned" },
+  { "source": 0, "target": "shouldn't" },
+  { "source": 1, "target": "terms" },
+  { "source": 1, "target": "soundtrack" },
+  { "source": 1, "target": "college" },
+  { "source": 1, "target": "marvel" },
+  { "source": 1, "target": "crisis" },
+  { "source": 1, "target": "issue" },
+  { "source": 0, "target": "protracted" },
+  { "source": 0, "target": "borderline" },
+  { "source": 0, "target": "rap" },
+  { "source": 0, "target": "tended" },
+  { "source": 0, "target": "troubling" },
+  { "source": 0, "target": "cumbersome" },
+  { "source": 0, "target": "simpering" },
+  { "source": 0, "target": "harvard" },
+  { "source": 1, "target": "brutal" },
+  { "source": 1, "target": "stirring" },
+  { "source": 0, "target": "extended" },
+  { "source": 1, "target": "songs" },
+  { "source": 0, "target": "italian-language" },
+  { "source": 1, "target": "stirring" },
+  { "source": 1, "target": "throws" },
+  { "source": 1, "target": "blues" },
+  { "source": 1, "target": "piano" },
+  { "source": 1, "target": "beach" },
+  { "source": 1, "target": "stereotyped" },
+  { "source": 1, "target": "laugh-out-loud" },
+  { "source": 0, "target": "bedroom" },
+  { "source": 1, "target": "outstanding" },
+  { "source": 0, "target": "strings" },
+  { "source": 0, "target": "drown" },
+  { "source": 0, "target": "tinny" },
+  { "source": 1, "target": "fish-out-of-water" },
+  { "source": 0, "target": "pre-credit" },
+  { "source": 1, "target": "judith" },
+  { "source": 1, "target": "zaza's" },
+  { "source": 0, "target": "executed" },
+  { "source": 0, "target": "insulting" },
+  { "source": 1, "target": "ensures" },
+  { "source": 1, "target": "fluid" },
+  { "source": 1, "target": "arc" },
+  { "source": 0, "target": "ratchets" },
+  { "source": 0, "target": "ear-pleasing" },
+  { "source": 1, "target": "interpretation" },
+  { "source": 1, "target": "ecclesiastes" },
+  { "source": 0, "target": "hole-ridden" },
+  { "source": 0, "target": "loopy" },
+  { "source": 0, "target": "unending" },
+  { "source": 1, "target": "dramaturgy" },
+  { "source": 1, "target": "surge" },
+  { "source": 1, "target": "swirling" },
+  { "source": 0, "target": "legitimate" },
+  { "source": 1, "target": "carl" },
+  { "source": 0, "target": "moat" },
+  { "source": 1, "target": "ensnared" },
+  { "source": 0, "target": "borderline" }
+];
+
+
+    var nodes_sidf = {};
+
+    var maxWeight_sidf = 0;
+
+    data_sidf.forEach(function(d) {
+        d.source = nodes_sidf[d.source] || (nodes_sidf[d.source] = {name: d.source});
+        d.target = nodes_sidf[d.target] || (nodes_sidf[d.target] = {name: d.target});
+        if (d.weight > maxWeight_sidf) {
+            maxWeight_sidf = d.weight;
+        }
+    });
+
+    labels_sidf.forEach(function(d) {
+        d.source = nodes_sidf[d.source] || (nodes_sidf[d.source] = {name: d.source});
+        d.target = nodes_sidf[d.target] || (nodes_sidf[d.target] = {name: d.target});
+    })
+
+    var widthScale_sidf = d3.scaleLinear().domain(1, maxWeight_sidf).range([1, 5]);
+    var width_sidf = window.innerWidth * 4 / 5,
+        height_sidf = window.innerHeight * 4 / 4;
+
+    var force_sidf = d3.forceSimulation()
+                  .nodes(d3.values(nodes_sidf))
+                  .force("link", d3.forceLink(data_sidf).distance(150))
+                  .force("link", d3.forceLink(labels_sidf).distance(150))
+                  .force('center', d3.forceCenter(width_sidf / 2, height_sidf / 2))
+                  .force("x", d3.forceX())
+                  .force("y", d3.forceY())
+                  .force("charge", d3.forceManyBody().strength(-100))
+                  .alphaTarget(1)
+                  .on("tick", tick_sidf);
+
+    var svg_sidf = d3.select("#word-label-mr").append("svg")
+                .attr("width", width_sidf)
+                .attr("height", height_sidf);
+
+    var path2_sidf = svg_sidf.append("g")
+                 .selectAll("path")
+                 .data(labels_sidf)
+                 .enter()
+                 .append("path")
+                 .attr("stroke", "#FFC300")
+                 .attr("stroke-width", 1)
+                 .attr("fill", "none");
+
+    var node2_sidf = svg_sidf.selectAll(".node")
+     .data(force_sidf.nodes())
+     .enter().append("g")
+     .attr("class", "node")
+     .call(d3.drag()
+     .on("start", dragstarted_sidf)
+     .on("drag", dragged_sidf)
+     .on("end", dragended_sidf)
+    );
+
+    node2_sidf.append("circle")
+     .attr("r", 13)
+     .attr("fill", "white");
+
+    node2_sidf.append("text")
+     .style("text-anchor", "middle")
+     .text(function(item, index, array) {return item.name})
+     .attr("fill", "black")
+     .attr("font-size", 10)
+     .attr("font-family", "sans-serif");
+
+    var path_sidf = svg_sidf.append("g")
+                 .selectAll("path")
+                 .data(data_sidf)
+                 .enter()
+                 .append("path")
+                 .attr("stroke", "steelblue")
+                 .attr("stroke-width", function(d) { return widthScale_sidf(d.weight)})
+                 .attr("fill", "none");
+
+    var node_sidf = svg_sidf.selectAll(".node")
+     .data(force_sidf.nodes())
+     .enter().append("g")
+     .attr("class", "node")
+     .call(d3.drag()
+     .on("start", dragstarted_sidf)
+     .on("drag", dragged_sidf)
+     .on("end", dragended_sidf)
+    );
+
+    node_sidf.append("circle")
+     .attr("r", 13)
+     .attr("fill", "white");
+
+    node_sidf.append("text")
+     .style("text-anchor", "middle")
+     .text(function(item, index, array) {return item.name})
+     .attr("fill", "black")
+     .attr("font-size", 10)
+     .attr("font-family", "sans-serif");
+
+    function tick_sidf() {
+     path_sidf.attr("d", function(d) {
+         var dx = d.target.x - d.source.x,
+             dy = d.target.y - d.source.y;
+         return "M" + d.source.x + " " + d.source.y + " " + "L" + d.target.x + " " + d.target.y;
+     });
+
+     path2_sidf.attr("d", function(d) {
+         var dx = d.target.x - d.source.x,
+             dy = d.target.y - d.source.y;
+         return "M" + d.source.x + " " + d.source.y + " " + "L" + d.target.x + " " + d.target.y;
+     });
+
+     node_sidf.attr("transform", function(d) {
+     return "translate(" + d.x + "," + d.y + ")"; })
+
+     node2_sidf.attr("transform", function(d) {
+     return "translate(" + d.x + "," + d.y + ")"; })
+    };
+
+    function dragstarted_sidf(d) {
+     if (!d3.event.active) force_sidf.alphaTarget(0.3).restart();
+     d.fx = d.x;
+     d.fy = d.y;
+    };
+
+    function dragged_sidf(d) {
+     d.fx = d3.event.x;
+     d.fy = d3.event.y;
+    };
+
+    function dragended_sidf(d) {
+     if (!d3.event.active) force_sidf.alphaTarget(0);
+     if (d.fixed == true) { d.fx = d.x; d.fy = d.y; }
+     else { d.fx = null; d.fy = null; }
+    };
